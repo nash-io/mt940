60f8de048b0089ffa218e95eee5e3d1349d4af5a
Niels Grieger <niels.grieger@rockstardevelopers.de>
2019-06-29 20:16:45 +0200

    bump elixir version to 1.9 & update dependencies

--- mix.exs
+++ mix.exs
@@ -4,46 +4,28 @@ defmodule Mt940.Mixfile do
   def project do
     [
       app: :mt940,
-      version: "1.1.2",
-      elixir: "~> 1.3",
-      description: description(),
+      version: "1.1.3",
+      elixir: "~> 1.9",
+      description: "MT940/MT942 parser for Elixir.",
       package: package(),
       deps: deps(),
       name: "MT940/MT942 Parser",
-      source_url: "https://github.com/my-flow/mt940",
-      dialyzer: [plt_add_deps: true],
-      test_coverage: [tool: ExCoveralls],
-      consolidate_protocols: false
+      source_url: "https://github.com/my-flow/mt940"
     ]
   end
 
-
   def application do
-    [applications: [:logger]]
+    [extra_applications: [:logger]]
   end
-
 
   defp deps do
     [
-      {:credo,       "~> 0.5.3",  only: [:dev, :test]},
-      {:decimal,     "~> 1.3.1" },
-      {:earmark,     "~> 1.0.3",  only: :dev, override: true},
-      {:excoveralls, "~> 0.5.7",  only: :test},
-      {:exjsx,       "~> 3.2.1",  only: [:dev, :test]},
-      {:ex_doc,      "~> 0.14.3", only: :dev},
-      {:inch_ex,     "~> 0.5.5",  only: :docs},
-      {:timex,       "~> 3.1.5"},
-      {:tzdata,      "~> 0.1.8",  override: true}
+      {:credo, "~> 1.1.0"},
+      {:decimal, "~> 1.8.0"},
+      {:ex_doc, "~> 0.20.2", only: :dev},
+      {:timex, "~> 3.6.1"}
     ]
   end
-
-
-  defp description do
-    """
-    MT940/MT942 parser for Elixir.
-    """
-  end
-
 
   defp package do
     [
