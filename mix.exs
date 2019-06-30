4ad62a8e850cf29dc1647e48c53fdfd6d5e2d1fa
Niels Grieger <niels.grieger@rockstardevelopers.de>
2019-06-30 11:31:06 +0200

    bump version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mt940.Mixfile do
   def project do
     [
       app: :mt940,
-      version: "1.1.3",
+      version: "1.1.4",
       elixir: "~> 1.9",
       description: "MT940/MT942 parser for Elixir.",
       package: package(),
