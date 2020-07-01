941b3e921bf9fd33516a5726fdabf8a083897237
Rafael Scheffer <rafael@nash.io>
2020-07-01 16:33:10 -0300

    Bump version to 1.1.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mt940.Mixfile do
   def project do
     [
       app: :mt940,
-      version: "1.1.4",
+      version: "1.1.5",
       elixir: "~> 1.9",
       description: "MT940/MT942 parser for Elixir.",
       package: package(),
